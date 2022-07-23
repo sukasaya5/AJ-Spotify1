c8f1c08b08ff68bf672ae24744b2933ac0a6436d
sukasaya5 <108425035+sukasaya5@users.noreply.github.com>
2022-07-24 05:51:14 +0700

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,7 +1,7 @@
 param (
   [Parameter()]
   [switch]
-  $UninstallSpotifyStoreEdition = (Read-Host -Prompt 'Uninstall Spotify Windows Store edition if it exists (Y/N)') -eq 'y',
+  $UninstallSpotifyStoreEdition = (Read-Host -Prompt 'Uninstall Spotify Windows Store edition if it exists! (Y/N)') -eq 'y',
   [Parameter()]
   [switch]
   $UpdateSpotify,
